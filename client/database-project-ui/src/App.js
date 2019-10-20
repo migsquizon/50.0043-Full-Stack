985811ddca5e3e5c81d53ff9ee2fe18bdb5e0dbe
Kenneth Ng <noobkenneth@gmail.com>
2019-10-20 23:49:36 +0800

    Added BookInfo Container

--- client/database-project-ui/src/App.js
+++ client/database-project-ui/src/App.js
@@ -2,26 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import BookCard from './BookCard.js';
 import './App.css';
-import BookCarousel from './BookCarousel';
+import BookInfo from './BookInfo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <BookCarousel/>
-      </header>
+      <BookInfo />
     </div>
   );
 }
