085c7812be3d4aba7f13738c07a1fe0dc1ac8038
Miguel Canlas <migsquizon@yahoo.com>
2019-11-14 03:51:43 +0800

    implemented scripts

---
+++ scripts/full_stack.sh
@@ -0,0 +1,47 @@
+#ports
+sudo ufw enable
+
+sudo ufw allow 80
+
+
+# node
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash
+
+sudo apt-get install -y nodejs
+
+node --version
+
+npm --version
+
+# python
+sudo apt-get update
+
+sudo apt install python3-pip
+
+pip3 --version
+
+pip3 install -r requirements.txt
+
+#backend
+cd ..
+
+cd server
+
+python3 app.py nohup &
+
+yes
+
+#frontend
+cd ..
+
+cd client
+
+cd database-project-ui
+
+npm install
+
+npm start nohup &
+
+yes
+
+exit 0
