b3acf261854897b0e6b061e68d190516da570982
Miguel Canlas <migsquizon@yahoo.com>
2019-11-14 02:47:07 +0800

    update frontend script

--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -1,6 +1,6 @@
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash
+# curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash
 
-sudo apt-get install -y nodejs
+# sudo apt-get install -y nodejs
 
 # check versions
 node --version
