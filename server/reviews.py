fa1d249ef09616a08684943295f44ae427ecdf02
Gary Ong <garyong.ahs@gmail.com>
2019-11-18 23:22:32 +0800

    fixed sql

--- server/reviews.py
+++ server/reviews.py
@@ -28,7 +28,7 @@ def get_reviews(asin,num=5):
     """
     keep_alive()
     cursor = sql.cursor(dictionary=True)
-    cursor.execute("""SELECT * FROM `Reviews` where asin = %s LIMIT %s ORDER BY overall DESC""", (asin,num))
+    cursor.execute("""SELECT * FROM `Reviews` where asin = %s ORDER BY overall DESC LIMIT %s """, (asin,num))
     result = cursor.fetchall()
     return result
 
