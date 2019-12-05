ac2f9099f16281af57791ae5d922e1ad6cbdc083
Gary Ong <garyong.ahs@gmail.com>
2019-12-05 22:54:37 +0800

    api keys

--- client/database-project-ui/src/Register/Register.js
+++ client/database-project-ui/src/Register/Register.js
@@ -11,7 +11,7 @@ import * as yup from 'yup';
 import checkSvg from './checked.svg'
 require('dotenv/config');
 
-var CAPTCHA_SITE_KEY = process.env.REACT_APP_GOOGLE_CAPTCHA_SITE_KEY;
+var CAPTCHA_SITE_KEY = process.env.REACT_APP_GOOGLE_CAPTCHA_SITE_KEY | "6LdubLIUAAAAAAoBktfc0ZhDebacKayNIdqa0e3L";
 
 const schema = yup.object({
   firstName: yup
