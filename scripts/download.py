19d2bb1c1e5a84be711da62b38250ccad5033aa9
Miguel Canlas <migsquizon@yahoo.com>
2019-12-13 17:09:47 +0800

    json should work

--- scripts/download.py
+++ scripts/download.py
@@ -17,14 +17,13 @@ def download_sql():
 	cursor = sql.cursor(dictionary=True)
 	cursor.execute(""" SELECT * FROM `Reviews` """)
 	rows = cursor.fetchall()
-
 	fp = open('file.csv', 'w')
 	myFile = csv.writer(fp)
 	for row in rows:
-		# print(type(row))
-		print(row.values())
+	# print(type(row))
+	# print(row.values())
 		myFile.writerow(list(row.values()))
-		# print(feature)
+	# print(feature)
 	fp.close()
 
 
@@ -35,11 +34,21 @@ def download_sql():
 #    return json.dumps(doc, sort_keys=True, indent=4, default=json_util.default)
 
 def download_mongo():
-   metadata_collection = mongo['Kindle']['Metadata']
-   cursor = metadata_collection.find_one({},{'_id':False})
-   data =  cursor #json_util.dumps(cursor)
-   with open('datanew.json','w') as f:
-        json.dump(data,f)
+	metadata_collection = mongo['Kindle']['Metadata']
+	cursor = metadata_collection.find({},{'_id':False})
+	print('helllo')
+	data =  cursor #json_util.dumps(cursor)
+	print(data)
+	with open('datanew.json','w') as f:
+		for d in data:
+			print(d)
+			json.dump(d,f)
+
+	f.close()
+	return
+	# json
+	# for d in data:
+#   		json.dump(d,f)
 
 
 print('saving now')
--- scripts/hdfs_numpy.sh
+++ scripts/hdfs_numpy.sh
@@ -12,5 +12,9 @@ flintrock run-command --master-only databass_cluster \
 	yes | pip --no-cache-dir install pyspark --user &&\
 	yes | sudo yum install python34-setuptools -y &&\
 	yes | sudo easy_install-3.4 pip &&\
+	pip3 install mysql-connector-python --user &&\
+	pip3 install pymongo --user &&\
+	wget https://github.com/migsquizon/50.0043-Full-Stack/raw/automation/scripts/download.py
+	wget https://github.com/migsquizon/50.0043-Full-Stack/raw/automation/scripts/pearson.py
 	pip3 install flask --user"
 	
