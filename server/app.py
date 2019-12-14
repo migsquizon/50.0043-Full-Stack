20f6b7f1bbf4f1510456dbd97da8be41f1ad22a8
Gary Ong <garyong.ahs@gmail.com>
2019-12-15 01:25:52 +0800

    add secret_key

--- server/app.py
+++ server/app.py
@@ -3,3 +3,4 @@ import os
 app = Flask(__name__)
 app.config['MONGO_IP'] = os.environ.get('MONGO_IP')
 app.config['SQL_IP'] = os.environ.get('SQL_IP')
+app.config['SECRET_KEY'] = 'yckcmkg'#random secret key for dev purposes
